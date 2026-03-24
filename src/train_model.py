ccb2d99600ae961b36de68c2e7070352b199d011
Sakshi Kannor <sakshikannor@gmail.com>
2026-03-24 21:10:07 +0530

    Implement model training function using Logistic Regression

---
+++ src/train_model.py
@@ -0,0 +1,22 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+
+def train_model(df):
+
+    X = df.drop("Survived", axis=1)
+
+    y = df["Survived"]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+
+        X, y,
+        test_size=0.2,
+        random_state=42
+    )
+
+    model = LogisticRegression(max_iter=1000)
+
+    model.fit(X_train, y_train)
+
+    return model, X_test, y_test
